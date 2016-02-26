c92be37e6060047419fce351f42ee7a6a4ee57a7
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-26 01:12:55 +0100

    module specs

---
+++ tests/directive.spec.js
@@ -0,0 +1,13 @@
+var accounting = require('../libs/accounting');
+var curreny = require('../libs/banknote');
+
+describe('Bankroll Module', function() {
+    it('should pass', function() {
+        expect(true).toBe(true);
+    });
+    describe('Name of the group', function() {
+        it('should behave...', function() {
+            expect(false).toEqual(false);
+        });
+    });
+});
