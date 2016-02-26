91e79a24964bc52f7141356a4bea1306d2c20bea
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-26 01:12:38 +0100

    module source files

---
+++ src/bankroll.js
@@ -0,0 +1,33 @@
+angular.module('bankroll', [])
+    .directive('check', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, ele, attrs) {
+
+            }
+        };
+    }])
+    .directive('name', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, ele, attrs) {
+
+            }
+        };
+    }])
+    .directive('name', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, ele, attrs) {
+
+            }
+        };
+    }])
+    .directive('name', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, ele, attrs) {
+
+            }
+        };
+    }]);
